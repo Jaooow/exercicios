9113aa0a177507e5fe9dc6ac09f087c003938198
Jaooow <joaopedro_cc2017@hotmail.com>
2020-02-19 17:38:28 -0300

    Links de exercicios

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> Atividades - João Pedro Catarina Conçolaro </title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+        <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
+	</head>
+	<body>
+		<div class = "container-fluid">
+			<h1>Página com Links dos meus exercícios</h1>
+		</div>
+	</body>
+</html>
