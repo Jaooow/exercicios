e0e49e13e75f415bb953b17a0b69be74b1a58235
= <joaopedro_cc2017@hotmail.com.br>
2020-03-11 17:29:53 -0300

    Jogo da Velha

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-        <title> Atividades - João Pedro Catarina Conçolaro </title>
+        <title> Exercicos - João Pedro Catarina Conçolaro </title>
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
         <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
 		
@@ -25,6 +25,7 @@
 			<h1>Página com Links dos meus exercícios</h1>
 			
 			<a href = "20200219/home.html">Barbearia</a>
+			<a href = "20200311/jogo_da_velha.html">Jogo da Velha</a>
 		</div>
 	</body>
 </html>
